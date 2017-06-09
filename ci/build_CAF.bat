cb0289f892f2dc4a80717ebc49f28d86333818e1
DePizzottri <depizzottri@gmail.com>
2017-06-09 11:36:18 +0800

    CAF build bat

---
+++ ci/build_CAF.bat
@@ -0,0 +1,9 @@
+IF NOT EXISTS "C:/lib/actor-framework/build/lib (
+  git clone --branch 0.15.3 https://github.com/actor-framework/actor-framework.git
+  cd actor-framework
+  mkdir build
+  cd build
+  cmake %CMAKE_GENERATOR% -DCMAKE_GENERATOR_PLATFORM=%PLATFORM% -DCAF_NO_BENCHMARKS:BOOL=yes -DCAF_NO_EXAMPLES:BOOL=yes -DCAF_NO_MEM_MANAGEMENT:BOOL=yes -DCAF_NO_IO:BOOL=yes -DCAF_NO_UNIT_TESTS:BOOL=yes ..
+  msbuild CAF.sln /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM%
+  cd ../..
+)
