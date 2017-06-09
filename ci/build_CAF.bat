f621a5028b94ecd18c85210cf27079e6ce2f163f
DePizzottri <depizzottri@gmail.com>
2017-06-09 12:56:05 +0800

    restbed dependencies

--- ci/build_CAF.bat
+++ ci/build_CAF.bat
@@ -4,7 +4,7 @@ IF NOT EXIST "C:/lib/actor-framework/build/lib" (
   mkdir build
   cd build
   cmake %CMAKE_GENERATOR% -DCMAKE_GENERATOR_PLATFORM=%PLATFORM% -DCAF_NO_BENCHMARKS:BOOL=yes -DCAF_NO_EXAMPLES:BOOL=yes -DCAF_NO_MEM_MANAGEMENT:BOOL=yes -DCAF_NO_IO:BOOL=yes -DCAF_NO_UNIT_TESTS:BOOL=yes -DCAF_NO_OPENCL:BOOL=yes ..
-  msbuild CAF.sln /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM%
+  msbuild CAF.sln /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM% /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
   cd ../..
 )
 echo "Done building CAF"
--- ci/build_restbed.bat
+++ ci/build_restbed.bat
@@ -1,10 +1,10 @@
 IF NOT EXIST C:/lib/restbed/build/%CONFIGURATION% (
-  git clone --branch 4.5 https://github.com/Corvusoft/restbed.git
+  git clone  --recursive --branch 4.5 https://github.com/Corvusoft/restbed.git
   cd restbed
   mkdir build
   cd build
   cmake -G %CMAKE_GENERATOR% -DCMAKE_GENERATOR_PLATFORM=%PLATFORM% -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=NO ..
-  msbuild restbed.sln /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM%
+  msbuild restbed.sln /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM% /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
   cd ../..
 )
 
